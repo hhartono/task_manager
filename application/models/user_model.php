6e48b7729da10b97c69dffbd977a0acb82539502
Nugraha S. Fajar <nugie.nsf@gmail.com>
2015-10-06 14:17:14 +0700

    admin login

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -6,9 +6,9 @@ class User_model extends CI_Model {
     }
 
     public function get_user_by_name($name, $pass){
-        $query = $this->db->query("Select user.id as iduser, worker.id as idworker, worker.nama as nama, user.status as status
-                                    from user, worker
-                                    where user.worker_id = worker.id AND user.username ='$name' AND  user.password ='$pass'");
+        $query = $this->db->query("Select user.id as iduser, user.status as status
+                                    from user
+                                    where user.username ='$name' AND  user.password ='$pass'");
         return $query->row_array();
     }
 }
